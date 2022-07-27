eb94315ce34a36a88f73c95c7725741f33a32e89
xuxing <xhxx20009@outlook.com>
2022-07-27 20:54:15 +0800

    注册

--- src/main/java/com/cy/store/service/impl/IUserServiceImpl.java
+++ src/main/java/com/cy/store/service/impl/IUserServiceImpl.java
@@ -6,7 +6,13 @@ import com.cy.store.service.IUserService;
 import com.cy.store.service.ex.InsertException;
 import com.cy.store.service.ex.UsernameDuplicateException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Service
 public class IUserServiceImpl implements IUserService {
 
     @Autowired
@@ -21,9 +27,32 @@ public class IUserServiceImpl implements IUserService {
             throw new UsernameDuplicateException("用户名被占用");
         }
 
+        /**密码加密处理*/
+        String oldPassword = user.getPassword();
+        String salt = UUID.randomUUID().toString().toUpperCase();
+        //盐值的记录
+        user.setSalt(salt);
+        String md5Password = getMD5Password(oldPassword, salt);
+        user.setPassword(md5Password);
+
+        user.setIsDelete(0);
+        user.setCreatedUser(user.getUsername());
+        user.setModifiedUser(user.getUsername());
+        Date date = new Date();
+        user.setCreatedTime(date);
+        user.setModifiedTime(date);
+
         Integer rows = userMapper.insert(user);
         if (rows != 1) {
             throw new InsertException("在用户注册中产生了未知的异常");
         }
+    }
+
+    /**定义一个md5算法的加密处理*/
+    private String getMD5Password(String password, String salt) {
+        for (int i = 0; i < 3; i++) {
+            password = DigestUtils.md5DigestAsHex((salt+password+salt).getBytes()).toUpperCase();
+        }
+        return password;
     }
 }
